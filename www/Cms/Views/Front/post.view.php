aecdbddaa4188635869b445da38b3d901551afeb
jsaleix <julian.saleix@gmail.com>
2021-06-24 15:03:42 +0200

    post.view displays comments only if comments are allowed

--- www/Cms/Views/Front/post.view.php
+++ www/Cms/Views/Front/post.view.php
@@ -4,17 +4,20 @@
 <hr>
 <?php if(isset($errors) && !empty($errors)):?>
     <?php foreach ($errors as $error):?>
-	<li style="color:red"><?=$error;?></li>
-<?php endforeach;?>
+        <li style="color:red"><?=$error;?></li>
+    <?php endforeach;?>
 <?php endif ?>
-<?php if($canPostComment): ?>
-    <button onClick="toggleBox()">Publish a comment</button>
-    <form id="commentBox" action="" method="POST">
-        <input name="message"/>
-        <input type="submit" value="Publish"/>
-    </form>
-<?php else: ?>
-    <button>You must be logged in to post a comment</button>
+
+<?php if($post['allowComment']):?>
+    <?php if($canPostComment): ?>
+        <button onClick="toggleBox()">Publish a comment</button>
+        <form id="commentBox" action="" method="POST">
+            <input name="message"/>
+            <input type="submit" value="Publish"/>
+        </form>
+    <?php else: ?>
+        <button>You must be logged in to post a comment</button>
+    <?php endif;?>
 <?php endif;?>
 
 <?php if(isset($comments) && !empty($comments)):?>
@@ -22,7 +25,7 @@
         <p><?=$comment['message']?></p>
         <i>Published by <?= $comment['author'] ?> - <?=$comment['date']?></i>
         <p>###############</p>
-<?php endforeach;?>
+    <?php endforeach;?>
 <?php endif ?>
 
 <script>
