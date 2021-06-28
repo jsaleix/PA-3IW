5404445df74b67d17b5059c6add00cf5e6ecb4c5
jsaleix <julian.saleix@gmail.com>
2021-06-28 22:35:05 +0200

    fixed front tpl css issue / list.account view added / mysites displays owned + whitelisted sites

--- www/Controllers/Account.php
+++ www/Controllers/Account.php
@@ -27,41 +27,63 @@ class Account{
 	}
 
 	public function mysitesAction(){
-		if( Secu::isConnected() == 0 )
-			return false;
-		$wlistObj = new Whitelist();
-		$wlistObj->setIdUser(Secu::getUser());
-		if($wlistObj->getIdUser() == 0 )
+		if( !Secu::isConnected() )
 			header("Location:" . DOMAIN . '/login');
-		$wlists = $wlistObj->findAll();
+		
 		$siteObj = new Site();
-		$sites = [];
-		foreach($wlists as $wlist){
-			$siteObj->setId($wlist["idSite"]);
-			$site = $siteObj->findOne();
-			array_push($sites, $site);
+		$userObj = new User();
+		$lists = [];
+
+		//Fetching all the sites owned by the user
+		$siteObj->setCreator(Secu::getUser());
+		$ownedSites = $siteObj->findAll();
+		if($ownedSites){
+			$datas = [];
+			$fields = [ 'id', 'version', 'name', 'subDomain', 'creation date', 'prefix', 'type', 'visit','edit' ];
+			foreach($ownedSites as $item){
+                $userObj->setId($item['creator']);
+                $creator = $userObj->findOne();
+				$visitBtn = '<a href="'. DOMAIN . '/site/' . $item['subDomain'] . '">Go</a>';
+				$editBtn = '<a href="'. DOMAIN . '/site/' . $item['subDomain'] . '/admin/settings">Edit</a>';
+                $creatorBtn = '<a href="user?id=' .  $item['creator'] . '">' . $creator['firstname'] . ' ' . $creator['lastname']. '</a>';
+				$img = '<img src=' . DOMAIN . '/' . $item['image'] . ' width=100 height=80/>';
+				$formalized = "'" . $item['id'] . "','" . 0.0 . "','" . $item['name'] . "','" . $item['subDomain'] . "','" . $item['creationDate'] . "','". $item['prefix'] . "','". $item['type'] . "','" . $visitBtn . "','" . $editBtn . "'";
+				$datas[] = $formalized;
+			}
+			$lists[] = array( "title" => "Sites you own", "datas" => $datas, "id" => "owned_sites", "fields" => $fields );
 		}
 
+		//Fetching all the sites on which the user is whitelisted on
+		$wlistObj = new Whitelist();
+		$wlistObj->setIdUser(Secu::getUser());
+		$wlists = $wlistObj->findAll();
+		$sites = [];
+		if($wlists){
+			foreach($wlists as $wlist){
+				$siteObj->setId($wlist["idSite"]);
+				$site = $siteObj->findOne();
+				array_push($sites, $site);
+			}
+		}
 		$fields = [ 'id', 'version', 'name', 'creator', 'subDomain', 'creation date', 'prefix', 'type', 'visit','edit' ];
 		$datas = [];
-        $userObj = new User();
-		
+
 		if($sites){
 			foreach($sites as $item){
                 $userObj->setId($item['creator']);
                 $creator = $userObj->findOne();
 				$visitBtn = '<a href="'. DOMAIN . '/site/' . $item['subDomain'] . '">Go</a>';
-				$editBtn = '<a href="site?id=' . $item['id'] . '">Edit</a>';
+				$editBtn = '<a href="'. DOMAIN . '/site/' . $item['subDomain'] . '/admin/settings">Edit</a>';
                 $creatorBtn = '<a href="user?id=' .  $item['creator'] . '">' . $creator['firstname'] . ' ' . $creator['lastname']. '</a>';
 				$img = '<img src=' . DOMAIN . '/' . $item['image'] . ' width=100 height=80/>';
 				$formalized = "'" . $item['id'] . "','" . 0.0 . "','" . $item['name'] . "','" .$creatorBtn .  "','" . $item['subDomain'] . "','" . $item['creationDate'] . "','". $item['prefix'] . "','". $item['type'] . "','" . $visitBtn . "','" . $editBtn . "'";
 				$datas[] = $formalized;
 			}
 		}
-
-		$view = new View('front/list', 'front');
-		$view->assign("fields", $fields);
-		$view->assign("datas", $datas);
+		$lists[] = array( "title" => "Sites you are whitelisted on", "datas" => $datas, "id" => "shared_sites", "fields" => $fields );
+		//var_dump($lists[1]);
+		$view = new View('front/list.account', 'front');
+		$view->assign("lists", $lists);
 		$view->assign('pageTitle', "Manage the sites");
 	}
 
--- www/Views/Templates/front.tpl.php
+++ www/Views/Templates/front.tpl.php
@@ -8,12 +8,12 @@
 	<script src="/Assets/js/jquery-3.5.1.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
 	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+	<link rel="stylesheet" href=<?php echo STYLES ?>>
+
 </head>
 
 <body>
-	<header>
-		<h1>Template du front</h1>
-	</header>
+
 
 	<?php include $this->view ;?>
 
