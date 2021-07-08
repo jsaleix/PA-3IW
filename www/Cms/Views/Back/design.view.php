95bb1235e81af9825fce495c17ed517a389fc3da
Léo Clairet <leo.clairet@gmail.com>
2021-07-08 14:52:28 +0200

    Patch responsive style Design View

--- www/Cms/Views/Back/design.view.php
+++ www/Cms/Views/Back/design.view.php
@@ -2,13 +2,13 @@
 <hr/>
 
 <div class="row">
-    <div class ="col-6">
+    <div class ="col-6 col-md-12 col-sm-12" style="padding: 1em; padding-top:0;">
         <h2>Current theme inuse: <span style="color:#2DC091"><?= $site['theme']; ?></span></h2>
 
         <h3>Change theme to: </h3>
         <?php App\Core\FormBuilder::render($form)?>    
     </div>
-    <div class="col-6" style="padding: 1em; padding-top:0;">
+    <div class="col-6 col-md-12 col-sm-12" style="padding: 1em; padding-top:0;">
         <h3>Preview:</h3>
         <div class="thumbnail-slider">
             <div class="arrow-container">
