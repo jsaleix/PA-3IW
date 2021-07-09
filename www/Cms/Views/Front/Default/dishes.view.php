3924682746d9465b3158a2c9fa932b843bfad88b
Léo Clairet <leo.clairet@gmail.com>
2021-07-09 16:09:41 +0200

    Dish patch, Dish front style

--- www/Cms/Views/Front/Default/dishes.view.php
+++ www/Cms/Views/Front/Default/dishes.view.php
@@ -1,20 +1,24 @@
 <div class="main-container">
     <div class="col-8 dishes-container">
+        <h1>Dishes</h1>
+        <hr/>
         <?php if(isset($categories) && $categories && count($categories) > 0): ?>
             <?php foreach($categories as $category): ?>
-                <h1>Dishes</h1>
+                <h2><?= $category['category']['name'] ?></h2>
                 <hr/>
                 <?php if(count($category['dishes']) > 0): ?>
-                        <div class="dish-row">
-                            <?php foreach($category['dishes'] as $dish): ?>
-                                <div class="dish col-3 col-sm-12">
+                    <div class="row">
+                        <?php foreach($category['dishes'] as $dish): ?>
+                            <div class="dish-col col-3 col-sm-12">
+                                <div class="dish">
                                     <a href="ent/dish?id=<?= $dish['id'] ?>" ><img alt="Dish" src='<?=  DOMAIN . '/' . $dish['image'] ?>'/></a>
                                     <span class="dish-name"><?= $dish['name'] ?></span>
-                                    <span class="dish-category"><?= $dish['category']['name'] ?></span>
                                     <span class="dish-price"><?= $dish['price'] ?>$</span>
                                 </div>
-                            <?php endforeach; ?>
-                        </div>
+                            </div>
+                            
+                        <?php endforeach; ?>
+                    </div>
                 <?php endif; ?>
             <?php endforeach;?>
         <?php endif; ?>
