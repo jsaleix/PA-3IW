92d2149c55c7252a0006a8003952f0922942cdce
Léo Clairet <leo.clairet@gmail.com>
2021-07-09 15:44:02 +0200

    Patch Dish add/edit name problems, images problems

--- www/Cms/Models/Dish.php
+++ www/Cms/Models/Dish.php
@@ -30,8 +30,7 @@ class Dish extends CMSModels
 	public function setName($name){
 		$name = htmlspecialchars($name);
         //$name = preg_replace("/\s+/", "", $name);//removes spaces
-        $name = preg_replace("/[^A-Za-z0-9]+/", "", $name);//keeps letters and digits
-
+        //$name = preg_replace("/[^A-Za-z0-9]+/", "", $name);//keeps letters and digits
         $this->name = $name;
 	}
 
--- www/Cms/Views/Back/createDish.view.php
+++ www/Cms/Views/Back/createDish.view.php
@@ -1,73 +1,58 @@
 <form method="POST" id="form_content" class="form-content" action="" enctype="multipart/form-data">
-        <div class="row" >
-            <div class="col-12 col-sm-12 col-md-12 col-xl-12">
-                <div class="col-inner">
-                    <div class="pageTitle">
-                        <h2 style="font-weight: lighter;"><?=$pageTitle?></h2>
-                    </div>
-                </div>
+        
+    <div class="col-12 col-sm-12 col-md-12">
+        <div class="pageTitle">
+            <h2 style="font-weight: lighter;"><?=$pageTitle?></h2>
+        </div>
+    </div>
+
+    <?php if(isset($errors)):?>
+
+    <?php foreach ($errors as $error):?>
+        <li style="color:red"><?=$error;?></li>
+    <?php endforeach;?>
+
+    <?php endif;?>
+
+    <?php if(isset($message)):?>
+        <h3> <?=$message?> </h3>
+    <?php endif;?>
+
+    <div class="row" style="justify-content: space-between;">
+        <div class="col-4 col-sm-12 col-md-12">
+            <div class='input-banner-container'>
+                <?php if(isset($image)):?>
+                    <img src="<?=$image?>"/>
+                    <label for="dish-select-image">Change image</label>
+                <?php else:?>
+                    <img src="<?= DOMAIN."/Assets/images/dish-select.png" ?>" style="width: 100%;" />
+                    <label for="dish-select-image">Set Dish Image</label>
+                <?php endif;?>
+                <input id="dish-select-image" name="image" type="file" class="cta-green"/>
             </div>
+            <br/>
+            <input autocomplete="off" name="allergens" id="remarksInput" class="input input-100" type="text" placeholder="Allergènes" value="<?=$allergens??''?>"/>
+            
+        </div>
+        <div class="col-7 col-sm-12 col-md-12">
+            <input class="input input-100" name="name" type="text" placeholder="Name" value="<?=$name??''?>"/>
+            <input  name="description" class="input input-100" type="text" placeholder="Description" value="<?=$description??''?>"/>
+            <div class="alignedInputs">
+                <input class="input" name="price" type="number" placeholder="Price" step="0.1" value="<?=$price?>"/>
+                <select class="input" name="category">
+                    <option value="0">None</option>
+                        <?php foreach ($categories as $key => $value):?>
+                            <?php if(isset($category)):?>
+                                <option value='<?=$key?>' <?= $key == $category ? 'selected' : '' ?>><?=$value?></option>
+                            <?php else: ?>
+                                <option value='<?=$key?>'><?=$value?></option>
+                            <?php endif; ?>
+                        <?php endforeach;?>
+                </select>
+            </div>
+            <input class="input input-100" name="notes" type="text" placeholder="Notes" value="<?=$notes??''?>"/>
+            <button type="submit" class="btn btn-light btn-100" >Add / Edit</button>
         </div>
 
-        <?php if(isset($errors)):?>
-
-        <?php foreach ($errors as $error):?>
-            <li style="color:red"><?=$error;?></li>
-        <?php endforeach;?>
-
-        <?php endif;?>
-
-        <?php if(isset($message)):?>
-            <h3> <?=$message?> </h3>
-        <?php endif;?>
-
-
-        <div class="row" >
-            <div class="col-4 col-sm-12 col-md-12 col-xl-4">
-                <div class="col-inner">
-                    <div class="darkSection">
-                        <?php if(isset($image)):?>
-                            <img src="<?=$image?>" style="width: 100%;" />
-                            <label>Change image</label>
-                        <?php else:?>
-                            <label>Set Image</label>
-                        <?php endif;?>
-                        <input name="image" type="file" class="cta-green"/>
-                    </div>
-                </div>
-
-                <div class="col-inner">
-                    <div class="darkSection">
-                        <input name="allergens" id="remarksInput" class="longInput" type="text" placeholder="Allergènes" value="<?=$allergens??''?>"/>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-8 col-sm-12 col-md-12 col-xl-8">
-                <div class="col-inner">
-                    <div class="darkSection">
-                        <input name="name" type="text" placeholder="Nom" value="<?=$name??''?>"/>
-                        <input  name="description"  class="longInput" type="text" placeholder="Description" value="<?=$description??''?>"/>
-                        <div class="alignedInputs">
-                            <input name="price" type="number" placeholder="Price" step="0.1" value="<?=$price?>"/>
-                            <select name="category">
-                                <option value="0">None</option>
-                                    <?php foreach ($categories as $key => $value):?>
-                                        <?php if(isset($category) && count($category) > 0):?>
-                                            <option value='<?=$key?>' <?php $key === $category ? 'selected=selected' : '' ?>><?=$value?></option>
-                                        <?php else: ?>
-                                            <option value='<?=$key?>'><?=$value?></option>
-                                        <?php endif; ?>
-                                    <?php endforeach;?>
-                            </select>
-                        </div>
-                        <input class="longInput" name="notes" type="text" placeholder="Notes" value="<?=$notes??''?>"/>
-                        <div class="validateRow">
-                            <button type="submit" class="cta-white" >Add</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+    </div>
 </form>
