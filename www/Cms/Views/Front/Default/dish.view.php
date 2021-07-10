6e43d4796f8f43c6aeeb3abd8585a380a43192a2
Léo Clairet <leo.clairet@gmail.com>
2021-07-10 16:08:59 +0200

    Dish view style

--- www/Cms/Views/Front/Default/dish.view.php
+++ www/Cms/Views/Front/Default/dish.view.php
@@ -1,12 +1,30 @@
-<img id="dishImg" src="<?=DOMAIN . '/' . $dish['image'] ?>"/>
-<h4><?=$dish['name']?></h4>
-<p><?=$dish['description']?></p>
-<p><?=$dish['notes']?></p>
-<p><?=$dish['allergens']?></p>
-<p>$<?=$dish['price']?></p>
+<main class="main-container">
+    <div class="row dish-container">
+        <div class="col-6 col-sm-12">
+            <img class="dish-img" alt="Dish image" src="<?=DOMAIN . '/' . $dish['image'] ?>"/>
+        </div>
+        <div class="col-6 col-sm-12 dish-col">
+            <div>
+                <div class="row" style="justify-content: flex-end;">
+                    <h1><?=preg_replace("/\\\+/", "", $dish['name'])?></h1>
+                </div>
+                <div class="row" style="justify-content: flex-end; padding-top: 0;">
+                    <p><?=$dish['description']?></p>
+                </div>
+                <hr/>
+                <?php if($dish['notes'] != ""):?>
+                    <span><b>Composition:</b> <span class="min"><?=$dish['notes']?></span></span>
+                <?php endif;?>
+                <br/>
+                <br/>
+                <?php if($dish['allergens'] != ""):?>
+                    <span><b>Allergènes:</b> <span class="min"><?=$dish['allergens']?></span></span>
+                <?php endif;?>
 
-<style>
-#dishImg{
-    width: 30%;
-}
-</style>
+            </div>
+            <h3 class="price"><?=$dish['price']?> $</h3>
+        </div>
+    </div>
+
+</main>
+
