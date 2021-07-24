cffb44bfe0367f8db5c4be8109980fa635a7148f
jsaleix <julian.saleix@gmail.com>
2021-07-24 14:15:52 +0200

    sitemap added in site head

--- www/Cms/Views/Templates/front.tpl.php
+++ www/Cms/Views/Templates/front.tpl.php
@@ -4,8 +4,8 @@
 	<meta charset="UTF-8">
 	<title><?=$pageTitle?></title>
 	<meta name="EasyMeal - Y restaurant's">
+	<link rel="sitemap" type="application/xml" title="Sitemap" href="<?= \App\Core\Helpers::renderCMSLink( "ent/sitemap.xml", $this->site) ?>">
     <?= CMS\Core\StyleBuilder::renderStyle($this->site) ?>
-	
 </head>
 <body>
 	<?= CMS\Core\navbarBuilder::renderFrontNavigation($this->site) ?>
